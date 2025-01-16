2a8ab2739c4489aaa040afc61897896fadfbcfbb
Makhuta <matykubla@gmail.com>
2025-01-16 22:56:21 +0100

    Update
    - removed necesity for HA to be exposed publicly to make the images work
    - removed the redirect because now it works with attaching the files (images) to the embed

--- custom_components/webhook_service/__init__.py
+++ custom_components/webhook_service/__init__.py
@@ -2,7 +2,6 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN, WEBHOOKS_DATAS
-from .redirect import ImagesRedirect
 
 import logging
 _LOGGER = logging.getLogger(__name__)
@@ -10,7 +9,6 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the integration."""
 
-    hass.http.register_view(ImagesRedirect(config))
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
         await _setup_webhooks(hass, config)
@@ -19,7 +17,6 @@ async def async_setup(hass: HomeAssistant, config: dict):
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up the platform."""
 
-    hass.http.register_view(ImagesRedirect(entry))
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
         await _setup_webhooks(hass, entry)
--- custom_components/webhook_service/webhook_functions/discord.py
+++ custom_components/webhook_service/webhook_functions/discord.py
@@ -6,15 +6,14 @@ from .. import const
 
 _LOGGER = logging.getLogger(__name__)
 
-def discord_webhook(hass, call):
+def run(hass, call, thumbnail_file=None, thumbnail_name=None, image_file=None, image_name=None):
     data = call.data.copy()
     title = data.get("title", "Embed Title")
     title_url = data.get("title_url")
     description = data.get("description", "Embed Description")
-    thumbnail = data.get("thumbnail")
+    
     author = data.get("author")
     fields = data.get("fields")
-    image = data.get("image")
     color = data.get("color")
     timestamp = data.get("timestamp")
     footer = data.get("footer")
@@ -30,8 +29,6 @@ def discord_webhook(hass, call):
 
     if title_url:
         output_data["url"] = title_url
-    if thumbnail:
-        output_data["thumbnail"] = thumbnail
     if author:
         try:
             output_data["author"] = json.loads(str(author))
@@ -42,23 +39,56 @@ def discord_webhook(hass, call):
             output_data["fields"] = json.loads(str(fields))[:25]
         except Exception as e:
             _LOGGER.error(e)
-    if image:
-        output_data["image"] = { "url": image }
     if color:
         output_data["color"] = color
     if timestamp:
         output_data["timestamp"] = timestamp
     if footer:
         output_data["footer"] = footer
+    
+
+    files = {}
+    if image_file and image_name:
+        files["file_image"] = (image_name, image_file)
+        output_data["image"] = {"url": f'attachment://{image_name}'}
+
+        
+    if thumbnail_file and thumbnail_name:
+        files["file_thumbnail"] = (thumbnail_name, thumbnail_file)
+        output_data["thumbnail"] = {"url": f'attachment://{thumbnail_name}'}
+
 
     embed_data = {
         "embeds": [output_data]
     }
 
-    result = requests.post(webhook_url, json=embed_data)
+    result = requests.post(webhook_url, data={"payload_json": json.dumps(embed_data)}, files=files)
 
     try:
         result.raise_for_status()
         _LOGGER.info("Embed sent successfully.")
     except:
         _LOGGER.error("Failed to send embed: %s", result.text)
+
+def discord_webhook(hass, call):
+    data = call.data.copy()
+    thumbnail = data.get("thumbnail")
+    image = data.get("image")
+
+
+    if thumbnail:
+        thumbnail_name = thumbnail.split("/")[-1]
+        with open(thumbnail, "rb") as thumbnail_file:
+            if image:
+                image_name = image.split("/")[-1]
+                with open(image, "rb") as image_file:
+                    run(hass, call, thumbnail_file, thumbnail_name, image_file, image_name)
+            else:
+                run(hass, call, thumbnail_file, thumbnail_name)
+
+
+
+    if not thumbnail and image:
+        image_name = image.split("/")[-1]
+        with open(image, "rb") as image_file:
+            run(hass, call, image_file=image_file, image_name=image_name)
